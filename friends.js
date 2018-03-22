495467cf496f8c7f22e50b3469560ccd8157dbf3
Poncho128 <alfonsoruvera@gmail.com>
2018-03-22 10:17:14 -0600

    Create friends.js
    Create remote json for mobiles app homework

---
+++ friends.js
@@ -0,0 +1,23 @@
+[
+  {
+    "name":"Marco",
+    "hobby":"sex",
+    "age":"69",
+    "phone":"0100011111",
+    "address":"The friendly corner #22"
+  },
+  {
+    "name":"Edgar",
+    "hobby":"Waifus",
+    "age":"42",
+    "phone":"3333333",
+    "address":"Waifus paradise #6969"
+  },
+    {
+    "name":"Juane",
+    "hobby":"Lolis",
+    "age":"30",
+    "phone":"40740740",
+    "address":"Right side from god #777"
+  }
+]
